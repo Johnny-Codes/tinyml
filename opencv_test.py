b7003aeb6d1394a65a6ad1e4ab31a31d53d0c19b
paul <johnspaulm@gmail.com>
2024-11-06 21:46:16 -0600

    capture video image and displays

--- opencv_test.py
+++ opencv_test.py
@@ -1,42 +1,24 @@
-from imutils.video import VideoStream
-import numpy as np
-import argparse
-import imutils
-import time
 import cv2
-from urllib.request import urlopen
-from PIL import Image
-import timm
-import torch
 
-with open('image_net_labels', 'r') as f:
-    content = f.read()
-
-image_net_labels = eval(content)
-
-model = timm.create_model('mobilenetv4_hybrid_medium.e200_r256_in12k_ft_in1k', pretrained = True)
-model = model.eval()
-
-data_config = timm.data.resolve_model_data_config(model)
-transforms = timm.data.create_transform(**data_config, is_training = False)
-
-vs = VideoStream(usePiCamera=True).start()
-time.sleep(2.0)
+# Open a connection to the webcam (0 is the default camera)
+cap = cv2.VideoCapture(0)
 
 while True:
-    frame = vs.read()
-    frame = imutils.resize(frame, width=400)
+    # Capture frame-by-frame
+    ret, frame = cap.read()
 
-    img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-    output = model(transforms(img).insqueeze(0))
+    # If frame is read correctly, ret is True
+    if not ret:
+        print("Failed to grab frame")
+        break
 
-    top5_probabilities, top5_class_indices = torch.topk(output.softmax(dim=1) * 100, k = 5)
+    # Display the resulting frame
+    cv2.imshow("Webcam", frame)
 
-    print("Top 5")
-    print(top5_probabilities)
-    print(top5_class_indices)
+    # Press 'q' on the keyboard to exit the loop
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
 
-    predictions = []
-    for i in range(5)
-        try:
-            predictions.append([image_net_labels
+# When everything is done, release the capture
+cap.release()
+cv2.destroyAllWindows()
